1deb3117982d929e34ba04af724a3022024440a4
Mostafa Nasrollahpour <iammostafanasrollahpour@gmail.com>
2025-01-17 13:12:10 +0330

    add ok , unexpected to response file

--- Backend/response.py
+++ Backend/response.py
@@ -2,3 +2,12 @@ UserExist = {
     "is_succes": False,
     "detail": "User Name Already Exist"
 }
+
+OK = {
+    "is_succes": True
+}
+
+UnExpected = {
+    "is_succes": False,
+    "detail": "Unexpected error occurs"
+}
